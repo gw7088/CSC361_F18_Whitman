2e7195dc20890271a7da24abbb8149ede178342e
gw7088 <gwhitman55@gmail.com>
2018-10-21 20:51:32 -0400

    Added Updates to WorldRenderer/WorldController

--- core/src/com/packtpub/libgdx/flyordie/util/Constants.java
+++ core/src/com/packtpub/libgdx/flyordie/util/Constants.java
@@ -8,10 +8,19 @@ public class Constants
 	// Visible game world is 5 meters tall
 	public static final float VIEWPORT_HEIGHT =	5.0f;
 	
+	// GUI width
+	public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+	
+	// GUI height
+	public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+	
 	// Location of description file for texture atlas
 	public static final String TEXTURE_ATLAS_OBJECTS =
-		"images/flyordie.atlas";
+		"images/canyonbunny.atlas";
 	
 	// Location of image file for level 01
 	public static final String LEVEL_01 = "levels/level-01.png";
+	
+	// Amount of extra lives at level start
+	public static final int LIVES_START = 3;
 }
